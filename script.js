49706c84133a7a5aaf95f5631daa65d679f16f07
gracekabaghe <gracema2002@gmail.com>
2021-10-21 15:50:26 +0200

    Added styling on about page, and mobile versions

--- script.js
+++ script.js
@@ -8,11 +8,9 @@ function toggleMenu() {
   if (menu.classList.contains('showMenu')) {
     menu.classList.remove('showMenu');
     closeIcon.style.display = 'none';
-    menuIcon.style.display = 'block';
   } else {
     menu.classList.add('showMenu');
     closeIcon.style.display = 'block';
-    menuIcon.style.display = 'none';
   }
 }
 hamburger.addEventListener('click', toggleMenu);
@@ -22,3 +20,47 @@ menuItems.forEach(
     menuItem.addEventListener('click', toggleMenu);
   },
 );
+
+// teachers or featured speakers
+const data = [
+  {
+    image: 'images/pe-t.png',
+    teacherName: 'Ms. Pam',
+    subject: 'Level 1 Physical Science',
+    about: 'Joined ABS in January 2021. Besides teaching he enjoys playing the piano',
+  },
+  {
+    image: 'images/math-t.png',
+    teacherName: 'Ms. Jane',
+    subject: 'Level 1 Geography teacher',
+    about: 'Joined ABS in January 2021. Besides teaching he enjoys playing the piano',
+  },
+  {
+    image: 'images/geog-t.png',
+    teacherName: 'Ms. Cathy',
+    subject: 'Level 1 Physical Science',
+    about: 'Joined ABS in January 2021. Besides teaching he enjoys playing the piano',
+  },
+  {
+    image: 'images/art-t.png',
+    teacherName: 'Ms. Molly',
+    subject: 'Level 1 Physical Science',
+    about: 'Joined ABS in January 2021. Besides teaching he enjoys playing the piano',
+  },
+];
+
+const mainCard = document.querySelector('.desktop-all-staff');
+for (let i = 0; i < data.length; i += 1) {
+  const teacher = data[i];
+  mainCard.innerHTML += `
+        <div class="staff-members">
+            <img class="science-t-pic" src="${teacher.image}" alt="science teacher">
+                <ul class="science-teacher-ul">
+                    <li class="name">${teacher.teacherName}</li>
+                    <li class="subject">${teacher.subject}</li>
+                    <li class="hr"><hr></li>
+                    <li class="about">${teacher.about}</li>
+                </ul>
+        </div>
+    `;
+}
